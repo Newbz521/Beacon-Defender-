439634490471332946c48c32c5a3c2eaff16b763
newbz521 <lawrence.yee1991@gmail.com>
2022-10-10 17:54:57 -0400

    mvp

---
+++ script.js
@@ -0,0 +1,119 @@
+let gameScreen = document.body;
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft") {
+    // console.log('left');
+    moveLeft();
+    // createBullet()
+  }
+  if (event.key === "ArrowRight") {
+    // console.log('right');
+    moveRight();
+    // createBullet()
+  }
+  if (event.key === "ArrowUp") {
+    // console.log("space")
+    moveUp();
+  }
+  if (event.key === "ArrowDown") {
+    // console.log("space")
+    moveDown();
+  }
+  if (event.key === " ") {
+    // console.log("space")
+    attack();
+  }
+});
+function createMap() {
+  let map = document.createElement("div");
+  map.classList.add("mapContainer");
+  map.style.display = "grid";
+  map.style.height = "80vh";
+  map.style.width = "80vh";
+  // map.style.transform = "rotateZ(45deg)";
+  gameScreen.append(map);
+
+  function generatePlatform(num) {
+    let platform = document.createElement("div");
+    platform.classList.add("platform");
+    platform.innerText = num;
+
+    map.append(platform);
+  }
+  for (let i = 0; i < 36; i++) {
+    generatePlatform(i);
+  }
+}
+createMap();
+
+function createPlayer(start) {
+  let player = document.createElement("div");
+  player.setAttribute("id", "player");
+  player.style.height = 5 + "vh";
+  player.style.width = 5 + "vh";
+  player.style.position = "absolute";
+  player.style.background = "red";
+  player.style.left = start.getBoundingClientRect().left;
+  player.style.top = start.getBoundingClientRect().top;
+  player.innerText = start.innerText;
+  start.appendChild(player);
+  let sword = document.createElement("div");
+  sword.classList.add("sword")
+  sword.setAttribute("id", "sword");
+  sword.style.height = start.getBoundingClientRect().width * 2 + "px";
+  player.appendChild(sword);
+}
+function placePlayer() {
+  let randomIndex = Math.floor(
+    Math.random() * document.querySelectorAll(".platform").length
+  );
+  let startPosition = document.querySelectorAll(".platform")[randomIndex];
+
+  console.log(startPosition);
+  createPlayer(startPosition);
+}
+
+placePlayer();
+
+function moveLeft() {
+  let currentPosition = document.querySelector("#player");
+  let platforms = document.querySelectorAll(".platform");
+  for (let i = 0; i < platforms.length; i++) {
+    if (platforms[i] === currentPosition.parentElement && i > 0) {
+      platforms[i].removeChild(currentPosition);
+      createPlayer(platforms[i - 1]);
+    }
+  }
+}
+
+function moveRight() {
+  let platforms = document.querySelectorAll(".platform");
+  let currentPosition = document.querySelector("#player");
+  for (let i = 0; i < platforms.length; i++) {
+    if (platforms[i] === currentPosition.parentElement && (i < platforms.length - 1)) {
+      platforms[i].removeChild(currentPosition);
+      createPlayer(platforms[i + 1]);
+    }
+  }
+}
+
+function moveUp() {
+  let platforms = document.querySelectorAll(".platform");
+  let currentPosition = document.querySelector("#player");
+  for (let i = 0; i < platforms.length; i++) {
+    if (platforms[i] === currentPosition.parentElement && (i > 6)) {
+      platforms[i].removeChild(currentPosition);
+      createPlayer(platforms[i - 6]);
+    }
+  }
+}
+
+function moveDown() {
+  let platforms = document.querySelectorAll(".platform");
+  let currentPosition = document.querySelector("#player");
+  for (let i = 0; i < platforms.length; i++) {
+    if (platforms[i] === currentPosition.parentElement && (i < 30)) {
+      platforms[i].removeChild(currentPosition);
+      createPlayer(platforms[i + 6]);
+    }
+  }
+}
