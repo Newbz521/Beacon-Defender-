9492d11b4ff69244bdfc647d588a8ed5d2d70988
newbz521 <lawrence.yee1991@gmail.com>
2022-10-11 10:16:33 -0400

    game logic complete

--- script.js
+++ script.js
@@ -27,18 +27,22 @@ function createMap() {
   let map = document.createElement("div");
   map.classList.add("mapContainer");
   map.style.display = "grid";
-  map.style.height = "80vh";
-  map.style.width = "80vh";
+  map.style.height = "95vh";
+  map.style.width = "95vh";
   // map.style.transform = "rotateZ(45deg)";
   gameScreen.append(map);
 
   function generatePlatform(num) {
     let platform = document.createElement("div");
     platform.classList.add("platform");
-    platform.innerText = num;
+    let number = document.createElement("div")
+    number.innerText = num;
+    number.classList.add("nums")
+    platform.appendChild(number);
 
     map.append(platform);
   }
+
   for (let i = 0; i < 36; i++) {
     generatePlatform(i);
   }
@@ -57,10 +61,15 @@ function createPlayer(start) {
   player.innerText = start.innerText;
   start.appendChild(player);
   let sword = document.createElement("div");
-  sword.classList.add("sword")
+  sword.classList.add("sword");
   sword.setAttribute("id", "sword");
   sword.style.height = start.getBoundingClientRect().width * 2.1 + "px";
   player.appendChild(sword);
+  let sword2 = document.createElement("div");
+  sword2.classList.add("sword2");
+  sword2.setAttribute("id", "sword2");
+  sword2.style.width = start.getBoundingClientRect().width * 2.1 + "px";
+  player.appendChild(sword2);
 }
 function placePlayer() {
   let randomIndex = Math.floor(
@@ -76,12 +85,13 @@ placePlayer();
 function moveLeft() {
   let currentPosition = document.querySelector("#player");
   let platforms = document.querySelectorAll(".platform");
+
   for (let i = 0; i < platforms.length; i++) {
-    if (platforms[i] === currentPosition.parentElement && (i % 6 !== 0)) {
+    if (platforms[i] === currentPosition.parentElement && i % 6 !== 0) {
+      // console.log(platforms[i].childNodes)
       platforms[i].removeChild(currentPosition);
       createPlayer(platforms[i - 1]);
       // platforms[i-1].appendChild(platforms[i].childNodes[1])
-
     }
   }
 }
@@ -92,7 +102,16 @@ function moveRight() {
   let platforms = document.querySelectorAll(".platform");
   let currentPosition = document.querySelector("#player");
   for (let i = 0; i < platforms.length; i++) {
-    if (platforms[i] === currentPosition.parentElement && (i < platforms.length - 1) && (i !== 5)  && (i !== 11)  && (i !== 17)  && (i !== 23)  && (i !== 29)  && (i !== 35)) {
+    if (
+      platforms[i] === currentPosition.parentElement &&
+      i < platforms.length - 1 &&
+      i !== (Math.sqrt(platforms.length) - 1) &&
+      i !== (Math.sqrt(platforms.length) * 2 - 1) &&
+      i !== (Math.sqrt(platforms.length) * 3 - 1) &&
+      i !== (Math.sqrt(platforms.length) * 4 - 1)&&
+      i !== (Math.sqrt(platforms.length) * 5 - 1)&&
+      i !== (Math.sqrt(platforms.length) * 6 - 1)
+    ) {
       platforms[i].removeChild(currentPosition);
       createPlayer(platforms[i + 1]);
     }
@@ -103,7 +122,7 @@ function moveUp() {
   let platforms = document.querySelectorAll(".platform");
   let currentPosition = document.querySelector("#player");
   for (let i = 0; i < platforms.length; i++) {
-    if (platforms[i] === currentPosition.parentElement && (i >= 6)) {
+    if (platforms[i] === currentPosition.parentElement && i >= (Math.sqrt(platforms.length))) {
       platforms[i].removeChild(currentPosition);
       createPlayer(platforms[i - 6]);
     }
@@ -114,7 +133,7 @@ function moveDown() {
   let platforms = document.querySelectorAll(".platform");
   let currentPosition = document.querySelector("#player");
   for (let i = 0; i < platforms.length; i++) {
-    if (platforms[i] === currentPosition.parentElement && (i < 30)) {
+    if (platforms[i] === currentPosition.parentElement && i < (platforms.length - Math.sqrt(platforms.length))) {
       platforms[i].removeChild(currentPosition);
       createPlayer(platforms[i + 6]);
     }
@@ -123,9 +142,61 @@ function moveDown() {
 
 function attack() {
   let sword = document.querySelector(".sword");
-  sword.classList.add("attack")
+  let sword2 = document.querySelector(".sword2");
+  sword.classList.add("attack");
+  sword2.classList.add("attack");
   function removeAttack() {
-    sword.classList.remove("attack")
+    sword.classList.remove("attack");
+    sword2.classList.remove("attack");
   }
-  setTimeout(removeAttack, 700)
+  setTimeout(removeAttack, 700);
+  let player = document.querySelector("#player");
+  let platforms = document.querySelectorAll(".platform");
+  let removed = [];
+  for (let i = 0; i < platforms.length; i++) {
+    let bombs = document.querySelectorAll(".ball");
+    if (platforms[i] == player.parentElement) {
+      let topRow = i - (Math.sqrt(platforms.length));
+      let bottomwRow = i + (Math.sqrt(platforms.length));
+      let left = i - 1;
+      let right = i + 1; 
+      let topLeftCorner = i - (Math.sqrt(platforms.length) + 1);
+      let topRightCorner = i - (Math.sqrt(platforms.length) - 1);
+      let bottomLeftCorner = i + (Math.sqrt(platforms.length)  - 1);
+      let bottomRightCorner = i + (Math.sqrt(platforms.length)  + 1);
+      
+      for (let j = 0; j < bombs.length; j++) {
+        if ((bombs[j].parentElement == platforms[i]) || (bombs[j].parentElement == platforms[left]) || (bombs[j].parentElement == platforms[right]) || (bombs[j].parentElement == platforms[topRow]) || (bombs[j].parentElement == platforms[bottomwRow]) || (bombs[j].parentElement == platforms[topLeftCorner]) || (bombs[j].parentElement == platforms[topRightCorner] || (bombs[j].parentElement == platforms[bottomLeftCorner])) || (bombs[j].parentElement == platforms[bottomRightCorner])) {
+          removed.push(bombs[j])
+          bombs[j].remove()
+        }
+      }
+    }
+  }
+  console.log(`${removed.length} bombs removed!` ) 
 }
+
+function createBomb() {
+  let randomIndex = Math.floor(
+    Math.random() * document.querySelectorAll(".platform").length
+  );
+  let startPosition = document.querySelectorAll(".platform")[randomIndex];
+  let bomb = document.createElement("div");
+  bomb.classList.add("ball");
+  bomb.classList.add("bubble")
+  bomb.setAttribute("id", "bomb");
+  startPosition.append(bomb);
+  function removeBomb() {
+    function shrink() {
+      bomb.classList.add("shrink")
+    }
+    function remove() {
+      bomb.remove()
+    }
+    setTimeout( shrink, 3000);
+    setTimeout(remove,4000)
+    }
+  removeBomb()
+}
+
+setInterval(createBomb, 1000);
